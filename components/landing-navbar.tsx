440f2f8f6d74f1a75fb1af13ebc0760c75e0ec66
Priyadarsh S S <priyadarsh2001@gmail.com>
2023-11-29 22:42:05 +0530

    Add UserButton component to LandingNavbar

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -3,7 +3,7 @@
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useAuth } from '@clerk/nextjs'
+import { UserButton, useAuth } from '@clerk/nextjs'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
@@ -45,6 +45,7 @@ export const LandingNavbar = () => {
             </Button>
           </Link>
         )}
+        <UserButton afterSignOutUrl='/' />
       </div>
     </nav>
   )
